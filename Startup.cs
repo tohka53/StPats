b508945c7b7aedcf3c5160d0390f5b4fd3edd088
tohka53 <mecg1994@gmail.com>
2018-09-03 10:00:46 -0600

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StPats.Startup))]
+namespace StPats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
